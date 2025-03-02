d0a25bc47fd7208e122b360559f633a3b2d23b32
TroncoNinja <mattcrafting25@gmail.com>
2025-03-02 21:55:58 +0100

    add Interface module with display and clear functions, and corresponding tests

---
+++ assets/Interface.lua
@@ -0,0 +1,73 @@
+Interface = {};
+Interface.__index = Interface;
+
+local windowSize = 62;  -- size of the writable space in the window
+local miniWindowSize = 14;  -- size of the writable space in the mini window
+local centralWindowSize = 28;   -- size of the writable space in the central window
+
+function Interface:new()
+    local instance = setmetatable({}, Interface);
+    return instance;
+end
+
+function Interface:write(player, enemy, message, rooms, roomIndex)
+
+    -- creation of the room strings for the interface
+    local rm1Text = "";
+    local rm3Text = "";
+    if roomIndex == 1 then
+        rm1Text = string.rep(" ", miniWindowSize);
+    else
+        rm1Text = " "..rooms[roomIndex - 1];
+        rm1Text = rm1Text .. string.rep(" ", miniWindowSize - string.len(rm1Text));
+    end
+    local rm2Text = " > "..rooms[roomIndex];
+    -- print("roomText: "..rm2Text);
+    -- print("roomLen:" .. string.len(rm2Text));
+    -- os.exit();
+    rm2Text = rm2Text .. string.rep(" ", miniWindowSize - string.len(rm2Text));
+    if roomIndex == #rooms then
+        rm3Text = string.rep(" ", miniWindowSize);
+    else
+        rm3Text = " "..rooms[roomIndex + 1];
+        rm3Text = rm3Text .. string.rep(" ", miniWindowSize - string.len(rm3Text));
+    end
+    
+
+    -- creation of the player stat strings for the interface
+    local strText = " STR: ".. player.str;
+    strText = strText .. string.rep(" ", miniWindowSize - string.len(strText));
+    local dexText = " DEX: ".. player.dex;
+    dexText = dexText .. string.rep(" ", miniWindowSize - string.len(dexText));
+    local conText = " HP:  ".. player.con;
+    conText = conText .. string.rep(" ", miniWindowSize - string.len(conText));
+
+    -- creation of the enemy stat strings for the interface
+    local enemyText = " "..enemy.name;
+    enemyText = enemyText .. string.rep(" ", centralWindowSize - string.len(enemyText));
+    local enemyHPText = " HP: ".. enemy.hp;
+    enemyHPText = enemyHPText .. string.rep(" ", centralWindowSize - string.len(enemyHPText));
+
+    -- creation of the ASCII interface
+    local returnASCII = 
+    "╔".."══════════════════════════════════════════════════════════════"..        "╗\n" ..
+    "║".."┌──────────────┐".."┌────────────────────────────┐".."┌──────────────┐".."║\n" ..
+    "║".."│"..strText .."│".."│ Stai affrontando:          │".."│"..rm1Text .."│".."║\n" ..
+    "║".."│"..dexText .."│".."│"..  enemyText           .."│".."│"..rm2Text .."│".."║\n" ..
+    "║".."│"..conText .."│".."│"..  enemyHPText         .."│".."│"..rm3Text .."│".."║\n" ..
+    "║".."└──────────────┘".."└────────────────────────────┘".."└──────────────┘".."║\n" ..
+    "╚".."══════════════════════════════════════════════════════════════"..        "╝\n" ..
+    message.."\n";
+
+    return returnASCII;
+end
+
+function Interface:clear()
+    if package.config:sub(1,1) == "\\" then
+        os.execute("cls");
+    else
+        os.execute("clear");
+    end
+end
+
+return Interface;
---
+++ tests/interface_tes.lua
@@ -0,0 +1,49 @@
+-- Test for the interface module
+
+-- require the interface module
+local Interface = require("assets.Interface")
+
+-- check if the Interface module was loaded
+if Interface == nil then
+    error("Failed to load Interface module")
+end
+
+-- create a new instance of the Interface class
+local interface = Interface:new()
+
+-- create a player, enemy, rooms placeholder
+local player = {
+    str = 10,
+    dex = 10,
+    con = 10
+}
+local enemy = {
+    name = "Goblin",
+    hp = 10
+}
+local noEnemy = {
+    name = "",
+    hp = ""
+}
+local rooms = {
+    "Castle",
+    "Prisons",
+    "Dungeon",
+    "Catacombs"
+}
+local message = "You are in a dark room, you can hear the sound of water dripping from the ceiling."
+local message2 = "the goblin rolls 2d6 and gets 7";
+local message3 = "the player rolls 2d6 and gets 12";
+
+-- test the Interface class
+Interface:clear()  -- Clear the console
+print(interface:write(player, enemy, message, rooms, 1))  -- Write the interface to the console
+os.execute("sleep 1")  -- Sleep for 1 second
+Interface:clear()  -- Clear the console
+print(interface:write(player, enemy, message2, rooms, 2))  -- Write the interface to the console
+os.execute("sleep 1")  -- Sleep for 1 second
+Interface:clear()  -- Clear the console
+print(interface:write(player, enemy, message3, rooms, 3))  -- Write the interface to the console
+os.execute("sleep 1")  -- Sleep for 1 second
+Interface:clear()  -- Clear the console
+print(interface:write(player, noEnemy, message, rooms, 4))  -- Write the interface to the console
